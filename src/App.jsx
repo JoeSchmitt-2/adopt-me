acf59264a723b04db516b7bdc6bdaeb04a016aee
JoeSchmitt-2 <84737443+JoeSchmitt-2@users.noreply.github.com>
2023-07-25 23:31:35 -0500

    added useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { StrictMode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
 
@@ -15,17 +17,20 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+    const adoptedPetHook = useState(null);
     return (
         <StrictMode>
             <BrowserRouter>
                 <QueryClientProvider client={queryClient}>
-                    <header>
-                        <Link to="/">Adopt Me!</Link>
-                    </header>
-                    <Routes>
-                        <Route path="/details/:id" element={<Details />} />
-                        <Route path="/" element={<SearchParams />} />
-                    </Routes>
+                    <AdoptedPetContext.Provider value={adoptedPetHook}>
+                        <header>
+                            <Link to="/">Adopt Me!</Link>
+                        </header>
+                        <Routes>
+                            <Route path="/details/:id" element={<Details />} />
+                            <Route path="/" element={<SearchParams />} />
+                        </Routes>
+                    </AdoptedPetContext.Provider>
                 </QueryClientProvider>
             </BrowserRouter>
         </StrictMode>
