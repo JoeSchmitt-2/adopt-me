c0100799b6db66fd81267a5ab219accf4164ae64
JoeSchmitt-2 <84737443+JoeSchmitt-2@users.noreply.github.com>
2023-08-01 15:11:18 -0500

    styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,33 @@ import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
 
 const queryClient = new QueryClient({
-    defualtOptions: {
-        queries: {
-            staleTime: Infinity,
-            cacheTime: Infinity
-        }
+  defualtOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
     }
+  }
 });
 
 const App = () => {
-    const adoptedPetHook = useState(null);
-    return (
-        <StrictMode>
-            <BrowserRouter>
-                <QueryClientProvider client={queryClient}>
-                    <AdoptedPetContext.Provider value={adoptedPetHook}>
-                        <header>
-                            <Link to="/">Adopt Me!</Link>
-                        </header>
-                        <Routes>
-                            <Route path="/details/:id" element={<Details />} />
-                            <Route path="/" element={<SearchParams />} />
-                        </Routes>
-                    </AdoptedPetContext.Provider>
-                </QueryClientProvider>
-            </BrowserRouter>
-        </StrictMode>
-    );
+  const adoptedPetHook = useState(null);
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPetHook}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </StrictMode>
+  );
 };
 
 const container = document.getElementById("root");
