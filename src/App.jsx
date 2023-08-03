f9f513c40b2e53860283ba7245ad6b4a52f4220d
JoeSchmitt-2 <84737443+JoeSchmitt-2@users.noreply.github.com>
2023-08-03 17:10:01 -0500

    store.js adoptedPetSlice.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import { StrictMode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import AdoptedPetContext from "./AdoptedPetContext";
+import { Provider } from "react-redux";
+import store from "./store";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
 
@@ -17,12 +17,11 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
-  const adoptedPetHook = useState(null);
   return (
     <StrictMode>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
-          <AdoptedPetContext.Provider value={adoptedPetHook}>
+          <Provider store={store}>
             <header>
               <Link to="/">Adopt Me!</Link>
             </header>
@@ -30,7 +29,7 @@ const App = () => {
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
-          </AdoptedPetContext.Provider>
+          </Provider>
         </QueryClientProvider>
       </BrowserRouter>
     </StrictMode>
